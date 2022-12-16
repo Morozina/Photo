8804c5356a20b8235077f7f9b5a583bc78b2383d
Vladlk <zaryadina2014@gmail.com>
2022-12-16 14:18:20 +0100

    no message

---
+++ PhotoTapps/PhotoCCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCCell.swift
+//  PhotoTapps
+//
+//  Created by Vladyslav Moroz on 08.11.2021.
+//
+
+import UIKit
+
+class PhotoCCell: UICollectionViewCell {
+    @IBOutlet weak var dogImageView: UIImageView!
+}
